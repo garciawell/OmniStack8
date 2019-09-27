4657680411e4c1fe51eb6a83eb86a8213c233b01
Wellington Garcia <garciawell@gmail.com>
2019-09-26 23:07:28 -0300

    Refactory project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const connectedUsers = {};
 
-io.on('connection', socket => {
+io.on("connection", socket => {
   const { user } = socket.handshake.query;
 
   connectedUsers[user] = socket.id;
 });
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-jxhrd.mongodb.net/omnistack8?retryWrites=true&w=majority', {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-0fzvd.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+// mongoose.connect("mongodb://localhost:27017/omini", {
+//   useNewUrlParser: true,
+//   useFindAndModify: true
+// });
 
 app.use((req, res, next) => {
   req.io = io;
